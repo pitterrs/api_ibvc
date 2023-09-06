0295285a3e85dd1107a3853ef3a0e953a0f19810
Pitter <pitterrs@gmail.com>
2023-09-05 22:44:43 -0300

    alterando para o banco de dados do servidor produtivo

--- connection.js
+++ connection.js
@@ -2,11 +2,21 @@ import mysql from "mysql";
 
 export const connection = mysql.createPool({
     connectionLimit : 100,
-    host: "localhost",
-    user: "root",
-    password: "root",
+    host: "177.11.48.200",
+    user: "ibvc_pitter",
+    password: "Oe,[$!S&5jYB",
     port: 3306,
-    database: "ibvc_data",
+    database: "ibvc_painel",
     multipleStatements: true,
     timezone: 'BRT'
 })
+// export const connection = mysql.createPool({
+//     connectionLimit : 100,
+//     host: "localhost",
+//     user: "root",
+//     password: "root",
+//     port: 3306,
+//     database: "ibvc_data",
+//     multipleStatements: true,
+//     timezone: 'BRT'
+// })
